c060abdcfad1fb3e5e0faeb6e8cf7132c913af34
Charles Fewlass <elbuagnin@gmail.com>
2024-08-08 14:31:55 -0500

    install pinia

--- src/main.ts
+++ src/main.ts
@@ -7,12 +7,15 @@ import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
-
-import App from './App.vue'
-
 const vuetify = createVuetify({
     components,
     directives,
 })
 
-createApp(App).use(vuetify).mount('#app')
+// Pinia
+import { createPinia } from 'pinia'
+const pinia = createPinia()
+
+import App from './App.vue'
+
+createApp(App).use(vuetify).use(pinia).mount('#app')
