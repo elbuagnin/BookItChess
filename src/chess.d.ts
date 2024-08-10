412cff60530a3a0e56a3c2a88575ce8a8b3b0d24
Charles Fewlass <elbuagnin@gmail.com>
2024-08-10 12:58:03 -0500

    undefined

--- src/chess.d.ts
+++ src/chess.d.ts
@@ -23,5 +23,5 @@ export type FEN_T = `{string & { __brand: "/^(?:(?:[PNBRQK]+|[1-8])\/){7}(?:[PNB
 //     color: Color
 // }
 import type * as Chess from "chess.js"
-export type SquareState = ({ square: Square; type: PieceSymbol; color: Color; } | null)
+export type SquareState = ({ square: Square; type: PieceSymbol; color: Color; } | null | undefined )
 export * from "chess.js"
